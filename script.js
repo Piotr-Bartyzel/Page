84746f02d44637a476ce5e7d20545fe4bcfc0393
Piotr-wilk <50671241+Piotr-wilk@users.noreply.github.com>
2020-03-05 01:43:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     $('html, body').stop().animate({scrollTop:400},500);
   })
-
+if($( window ).width()<=576){
    canvas = document.getElementById("cn");
 
    ctx = canvas.getContext("2d");
@@ -61,6 +61,7 @@ else{
   $(".menu").stop().animate({
       height: 100,
   },300)
+}
 }
 }
 });
